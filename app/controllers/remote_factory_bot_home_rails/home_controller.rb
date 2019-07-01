d632725bf056f3dc212dca9e2b4f65ab18d981ae
Jason Risch <jason@cardtapp.com>
2019-07-01 14:55:16 -0700

    Add auto factory loading in initial call

--- app/controllers/remote_factory_bot_home_rails/home_controller.rb
+++ app/controllers/remote_factory_bot_home_rails/home_controller.rb
@@ -7,6 +7,8 @@ module RemoteFactoryBotHomeRails
     skip_before_action *RemoteFactoryBotHomeRails.skip_before_action
     skip_around_action *RemoteFactoryBotHomeRails.skip_around_action
 
+    before_action :load_factories
+
     def create
       if RemoteFactoryBotHomeRails.enabled?
         resource = traits.empty? ? FactoryBot.create(factory, attributes) : FactoryBot.create(factory, *traits, attributes)
@@ -36,6 +38,28 @@ module RemoteFactoryBotHomeRails
 
     def traits
       attributes['traits'] || []
+    end
+
+    def load_factories
+      return if @factories_loaded || Rails.env.test?
+
+      self.class.find_definitions
+      @factories_loaded = true
+    end
+
+    def self.find_definitions
+    definition_file_paths = %w(factories test/factories spec/factories)
+      absolute_definition_file_paths = definition_file_paths.map { |path| File.join(Rails.root, path) }
+
+      absolute_definition_file_paths.uniq.each do |path|
+        load("#{path}.rb") if File.exist?("#{path}.rb")
+
+        if File.directory? path
+          Dir[File.join(path, "**", "*.rb")].sort.each do |file|
+            load file
+          end
+        end
+      end
     end
   end
 end
